bbe55af1f857eda3293c1f53c03dade4891fe1df
Miguel Castillo <manchagnu@gmail.com>
2015-07-12 21:52:42 -0400

    initial code check in

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # p-stream
-Stream to promise
+
+> Readable stream to Pormise
+
+Create Promises that resolve when a `readable` stream is finished streaming data. Or rejected when the stream reports an error.
+
+> Promise must be polyfilled.
+
+### Examples
+
+``` javascript
+var fs = require("fs");
+var pstream = require("p-stream");
+
+function readFile(filePath) {
+  return fs
+    .createReadStream(filePath)
+    .setEncoding("utf8");
+}
+
+pstream(readFile("somefile.log")).then(function(text) {
+  console.log("File read", text);
+});
+```
+
+> p-stream can handle strings and buffers.
+
+TIP: Make sure to set the encoding on the stream to properly handle utf8 strings. Please refer to [setEncoding ](https://nodejs.org/api/stream.html#stream_readable_setencoding_encoding) for more details.
---
+++ index.js
@@ -0,0 +1,69 @@
+var chunkTypes = {
+  "string": 1,
+  "object": 2
+};
+
+
+/**
+ * Takes in a readable stream and returns a promise. The promise is resolved
+ * when all the data is read. Or reject if the stream has errors.
+ *
+ * @param {Stream} stream - Stream to read from to resolve the promise with.
+ *
+ * @returns {Promise}
+ */
+function readableStream(stream) {
+  return new Promise(function(resolve, reject) {
+    var chunks = {
+      data: []
+    };
+
+    function onData(chunk) {
+      if (!chunks.type) {
+        chunks.type = chunkTypes[typeof chunk];
+      }
+
+      chunks.data.push(chunk);
+    }
+
+    function onEnd() {
+      if (chunks.type === chunkTypes.object) {
+        resolve(Buffer.concat(chunks.data));
+      }
+      else {
+        resolve(chunks.data.join());
+      }
+    }
+
+    function onError(error) {
+      reject(error);
+    }
+
+    stream
+      .on("error", onError)
+      .on("data", onData)
+      .on("end", onEnd);
+  });
+}
+
+
+/**
+ * Takes a stream and returns a promise that is resolved when the stream
+ * emitting data. Or rejected when the stream has an error.
+ *
+ * @param {Stream} stream - Stream to process
+ *
+ * @returns {Promise} Promise that is resolved when stream has no more
+ *  data or rejected if the stream has errors.
+ */
+function pstream(stream) {
+  if (stream.readable) {
+    return readableStream(stream);
+  }
+  else {
+    return Promise.resolve();
+  }
+}
+
+
+module.exports = pstream;
